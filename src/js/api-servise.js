36acd2349c82e24b4ce6062ac1b31150165a1c87
KirillKorotkiy <korotkiy210@gmail.com>
2022-06-14 16:21:55 +0300

    add new key

--- src/js/api-servise.js
+++ src/js/api-servise.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://newsapi.org/v2/everything?';
-const KEY = '42f37e3a97114a25a54e6b53dab9d050';
+const KEY = 'e5b74ef942424359907db3fcd031473c';
+const EXCLUDEDOMAINS =
+  'excludeDomains=gazeta.ru,exler.ru,news.pn,lenta.ru,rg.ru,vz.ru,meduza.io';
 export default {
   searchArt: '',
   page: 1,
   isLoading: false,
   fetchArticles() {
-    const filter = `excludeDomains=news.pn,lenta.ru,rg.ru,vz.ru,meduza.io&q=${this.query}&pageSize=7
+    const filter = `&q=${this.query}&${EXCLUDEDOMAINS}&pageSize=7
     &page=4&apiKey=${KEY}`;
     return axios
       .get(`${filter}`)
