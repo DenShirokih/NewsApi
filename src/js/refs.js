7206b8e52103ffab0da277c7c715c7e6bcc7b78f
Denis <abcdenis95@gmail.com>
2022-06-14 18:08:33 +0300

    add trending news

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,6 @@ const refs = {
   searchForm: document.querySelector('.search-form-js'),
   searchInput: document.querySelector('.js-input'),
   cardsList: document.querySelector('.js-cards__list'),
+  trendingNewsBtn: document.querySelector('.js-trending-news'),
 };
 export default refs;
--- src/js/search.js
+++ src/js/search.js
@@ -6,16 +6,22 @@ import itemCard from '../templates/item-card.hbs?raw';
 
 const submitForm = event => {
   event.preventDefault();
-
   const form = event.currentTarget;
   apiservise.query = form.elements.searchQuery.value;
-  apiservise.fetchArticles().then(fetchSucsess);
+  apiservise.NewsSearch().then(fetchSucsess);
   refs.cardsList.innerHTML = '';
 };
-refs.searchForm.addEventListener('submit', submitForm);
 
 const fetchSucsess = articles => {
   const arr = articles.slice(1);
   createCardNews(primaryCard, articles[0]);
   createCardNews(itemCard, arr);
 };
+const loadTrendihgNews = () => {
+  refs.cardsList.innerHTML = '';
+  apiservise.TrenringNews().then(fetchSucsess);
+};
+refs.trendingNewsBtn.addEventListener('click', loadTrendihgNews);
+refs.searchForm.addEventListener('submit', submitForm);
+
+loadTrendihgNews();
