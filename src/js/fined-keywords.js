dca094e026e0251b65882758d76ed58f46acf51d
Omelchenkormn <omelchenkormn@gmail.com>
2022-06-22 15:24:03 +0300

    total keywords mark

--- src/js/fined-keywords.js
+++ src/js/fined-keywords.js
@@ -4,21 +4,22 @@ import refs from './refs';
 const content = new Mark(document.querySelector(".js-content"));
 const keywordInput = document.querySelector(".js-content-input");
 function performMark() {
-  const keyword = keywordInput.value;
+    const keyword = keywordInput.value;
+    const options = {
+        "accuracy": {
+            "value": "exactly",
+            "limiters": [",", "."]
+        },
+        "done": function (counter) {
+            console.log(counter);
+        }
+        };
 
-  const options = {
-    "accuracy": {
-        "value": "exactly",
-        "limiters": [",", "."]
-    }
-  };
-
-  // const totalWord = keyword.reduce((sum, elem) => sum + elem, 0);
-  content.unmark({
-  	done: function(){
-    	content.mark(keyword,options);
-    }
-  });
+    content.unmark({
+        done: function () {
+            content.mark(keyword, options)
+        }
+    });
 };
 keywordInput.addEventListener("input", performMark);
 
